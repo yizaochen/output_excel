673ff603d5bcbfa36f94907e356fcb870ba42f19
yizaochen <yizaochen@gmail.com>
2024-08-10 17:31:58 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # output_excel
+
+- https://medium.com/@amirm.lavasani/how-to-structure-your-fastapi-projects-0219a6600a8f
---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
