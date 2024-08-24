8d607b31055c939ca9acd2d9a394e10fc685c956
yizaochen <yizaochen@gmail.com>
2024-08-24 15:35:15 +0800

    add fake_data

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,11 @@ from datetime import date
 
 # AR transaction record
 class CTBCForeignBankSlip(Base):
+    """
+    PredictType: ('A', 'B', 'D', 'E', 'F'), D is the special customer. In frontend, A light, E medium, F dark
+
+    """
+
     __tablename__ = "AutoAR_CTBCForeign_BankSlip"
 
     id: Mapped[int] = mapped_column("id", INTEGER, primary_key=True, autoincrement=True)
@@ -24,10 +29,10 @@ class CTBCForeignBankSlip(Base):
     FilePath: Mapped[str] = mapped_column("FilePath", TEXT, nullable=False)
     PredictType: Mapped[str] = mapped_column(
         "PredictType", TEXT, nullable=False
-    )  # ('A', 'B', 'C', 'D')
+    )  # ('A', 'B', 'D', 'E', 'F')
     ProcessStatus: Mapped[str] = mapped_column(
         "ProcessStatus", TEXT, nullable=False
-    )  # ('Processing', 'Fail', 'Finish', 'Cancel', 'Duplicate')
+    )  # ('Wait-For-Check', 'Finish')
     ReferenceNumber: Mapped[str] = mapped_column(
         "ReferenceNumber", TEXT, nullable=False
     )
@@ -37,3 +42,20 @@ class CTBCForeignBankSlip(Base):
     ArNo: Mapped[str] = mapped_column(
         "ArNo", TEXT, nullable=False
     )  # 入帳成功後寫入傳票號碼
+
+
+# This is the mapping table for SAP customer-id and customer-name
+class CTBCForeignSAP(Base):
+    __tablename__ = "AutoAR_CTBCForeign_SAP_map"
+
+    id = mapped_column("id", INTEGER, primary_key=True)
+    CustomerID = mapped_column("CustomerID", TEXT, nullable=False)
+    CustomerName = mapped_column("CustomerName", TEXT, nullable=False)
+
+
+# D-type customer
+class CTBCForeignSpecialCustomer(Base):
+
+    __tablename__ = "AutoAR_CTBCForeign_SpecialCustomer"
+    id = mapped_column("id", INTEGER, primary_key=True)
+    CustomerName = mapped_column("CustomerName", TEXT, nullable=False)
